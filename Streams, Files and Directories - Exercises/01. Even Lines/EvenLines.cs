781ae16f01385eb40de714b8438f6754aea1a06f
monikaez <86738350+monikaez@users.noreply.github.com>
2024-01-04 22:09:54 +0200

    04

---
+++ Streams, Files and Directories - Exercises/01. Even Lines/EvenLines.cs
@@ -0,0 +1,66 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using StreamReader streamReader = new(inputFilePath);
+
+            string line = string.Empty;
+            int count = 0;
+
+            StringBuilder sb = new();
+
+            while (line != null)
+            {
+                line = streamReader.ReadLine();
+
+                if (count % 2 == 0)
+                {
+                    string replaceSymbols = ReplaceSymbols(line);
+
+                    string reverseWord = ReverseWords(replaceSymbols);
+
+                    sb.AppendLine(reverseWord);
+                }
+                count++;
+            }
+            return sb.ToString();
+        }
+
+        private static string ReverseWords(string words)
+        {
+            string[] reversedWords = words.
+                Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+            return string.Join(" ", reversedWords);
+            
+        }
+
+        private static string ReplaceSymbols(string text)
+        {
+            StringBuilder sb = new(text);
+            char[] symbolToReplace = { '-', ',', '.', '!', '?' };
+
+            foreach (char symbol in symbolToReplace)
+            {
+                sb = sb.Replace(symbol, '@');
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercises/02. Line Numbers/LineNumbers.cs
@@ -0,0 +1,37 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder sb = new();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+            //Line 1: -I was quick to judge him, but it wasn't his fault. (37)(4)
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currLine = lines[i];
+
+                int lettersCount = currLine.Count(char.IsLetter);
+                int punctoSymbolCount = currLine.Count(char.IsPunctuation);
+
+                sb.AppendLine($"Line {i + 1}: -{currLine[i]} ({lettersCount})({punctoSymbolCount})");
+            }
+            File.WriteAllText(outputFilePath,sb.ToString());
+        }
+    }
+}
