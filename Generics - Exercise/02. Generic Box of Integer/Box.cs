aa5c3bbcac39cd955871a54d69fc15f5390fac57
monikaez <86738350+monikaez@users.noreply.github.com>
2024-01-04 22:33:36 +0200

    07

---
+++ Generics - Exercise/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.GenericBoxofInteger
+{
+    public class Box<T>
+    {
+        private List<T> items = new();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
